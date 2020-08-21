66c59727a40e66a74184baad6cac369bedc8832b
carloseduardo9959 <50346234+carloseduardo9959@users.noreply.github.com>
2020-08-20 22:12:35 -0300

    Revert "Alterações"

--- GUI_V_2/Calculos/frmTijolos.cs
+++ GUI_V_2/Calculos/frmTijolos.cs
@@ -22,49 +22,49 @@ namespace GUI_V_2.Calculos
             switch (metroComboBox1.SelectedIndex)
             {
                 case 0: //B12/29
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b12291;
+                    imgtijolo.Image = Properties.Resources.tijolo_b12291;
                     txtLarguraTijolo.Text = "11,5";
                     txtAlturaTijolo.Text= "19";
                     txtComprimentoTijolo.Text = "29";
                     break;
                 case 1: //B15/29
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b15291;
+                    imgtijolo.Image = Properties.Resources.tijolo_b15291;
                     txtLarguraTijolo.Text = "14";
                     txtAlturaTijolo.Text = "19";
                     txtComprimentoTijolo.Text = "29";
                     break;
                 case 2: //B15/39
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b15391;
+                    imgtijolo.Image = Properties.Resources.tijolo_b15391;
                     txtLarguraTijolo.Text = "14";
                     txtAlturaTijolo.Text = "19";
                     txtComprimentoTijolo.Text = "39";
                     break;
                 case 3: //B10/19
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b10191;
+                    imgtijolo.Image = Properties.Resources.tijolo_b10191;
                     txtLarguraTijolo.Text = "9";
                     txtAlturaTijolo.Text = "19";
                     txtComprimentoTijolo.Text = "19";
                     break;
                 case 4: //B10/29
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b10291;
+                    imgtijolo.Image = Properties.Resources.tijolo_b10291;
                     txtLarguraTijolo.Text = "9";
                     txtAlturaTijolo.Text = "19";
                     txtComprimentoTijolo.Text = "29";
                     break;
                 case 5: //B12/14/24
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b1214241;
+                    imgtijolo.Image = Properties.Resources.tijolo_b1214241;
                     txtLarguraTijolo.Text = "11,5";
                     txtAlturaTijolo.Text = "14";
                     txtComprimentoTijolo.Text = "24";
                     break;
                 case 6: //B10/20
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_b10201;
+                    imgtijolo.Image = Properties.Resources.tijolo_b10201;
                     txtLarguraTijolo.Text = "9";
                     txtAlturaTijolo.Text = "9";
                     txtComprimentoTijolo.Text = "19";
                     break;
                 case 7: //C5/20
-                    imgtijolo.Image = QuantConstruct.Properties.Resources.tijolo_c5201;
+                    imgtijolo.Image = Properties.Resources.tijolo_c5201;
                     txtLarguraTijolo.Text = "10";
                     txtAlturaTijolo.Text = "5";
                     txtComprimentoTijolo.Text = "20";
--- GUI_V_2/Properties/Resources.Designer.cs
+++ GUI_V_2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuantConstruct.Properties {
+namespace GUI_V_2.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace QuantConstruct.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QuantConstruct.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI_V_2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
